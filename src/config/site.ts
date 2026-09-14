8e6fe87da1174171310076d313f01c4a4a4a71a1
m7mmaz <qiqx70@gmail.com>
2026-09-15 00:13:17 +0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -77,7 +77,7 @@ export const LINKS = {
  * needs the embeddable player URL calls getYoutubeEmbedUrl(YOUTUBE_TUTORIAL_URL)
  * below rather than deriving/hardcoding it locally.
  */
-export const YOUTUBE_TUTORIAL_URL = "https://youtu.be/YCqgjkG2WOE";
+export const YOUTUBE_TUTORIAL_URL = "https://youtu.be/ondygIrgcWQ";
 
 /**
  * Accepts any of the common YouTube URL shapes a real video URL will arrive
